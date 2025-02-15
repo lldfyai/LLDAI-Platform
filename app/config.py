8e47698c70a54ee5cf657ed96b1c9a81f99bc26b
Adarsh2801-r <adarsh2801@gmail.com>
2025-02-15 12:55:11 +0530

    First pass baseline code

---
+++ app/config.py
@@ -0,0 +1,5 @@
+UPLOAD_DIR = "submission_uploads"
+LANGUAGE_CONFIG = {
+    "java": {"dockerfile": "Dockerfile.java", "image": "java_executor"},
+    "python": {"dockerfile": "Dockerfile.python", "image": "python_executor"},
+}
