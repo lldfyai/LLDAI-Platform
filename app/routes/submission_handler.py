5aed59ffa7e928431bbadb7876debb22d50b49db
Adarsh2801-r <adarsh2801@gmail.com>
2025-02-22 18:21:18 +0530

    Modify API request contract to accept files_metadata

--- app/routes/submission_handler.py
+++ app/routes/submission_handler.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File, Form
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 import os
 import shutil
@@ -6,10 +6,12 @@ import uuid
 from app.config import UPLOAD_DIR
 from app.services.code_executor import CodeExecutor
 from enum import Enum
+from pydantic import BaseModel
+
 
 router = APIRouter()
 
-class SubmissionStatus(Enum):
+class SubmissionState(Enum):
     RECEIVED = "Received"
     QUEUED = "Queued"
     COMPLETED = "Completed"
@@ -18,45 +20,40 @@ class SubmissionHandler:
     def __init__(self):
         self.executor = CodeExecutor()
 
-    async def save_files(self, files, user_folder):
+    async def save_files(self, files_metadata, user_folder):
+        # ToDo: Implement file saving logic in S3 bucket
         file_paths = []
-        for file in files:
-            file_path = os.path.join(user_folder, file.filename)
+        for filename, content in files_metadata.items():
+            file_path = os.path.join(user_folder, filename)
             with open(file_path, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
+                buffer.write(content.encode())
             file_paths.append(file_path)
         return file_paths
 
-    async def handle_submission(self, files, language, user_id, problem_id):
-        job_id = str(uuid.uuid4())
-        user_folder = os.path.join(UPLOAD_DIR, user_id, problem_id, job_id)
-        os.makedirs(user_folder, exist_ok=True)
-        print(f"Created user folder: {user_folder}")  # Debugging statement
+    async def handle_submission(self, files_metadata, language, user_id, problem_id):
+        submission_id = str(uuid.uuid4())
+        submission_folder = os.path.join(UPLOAD_DIR, user_id, problem_id, submission_id)
+        os.makedirs(submission_folder, exist_ok=True)
 
-        await self.save_files(files, user_folder)
+        await self.save_files(files_metadata, submission_folder)
 
-        for file in files:
-            file_path = os.path.join(user_folder, file.filename)
-            if not os.path.exists(file_path):
-                print(f"File not found: {file_path}")
-            else:
-                print(f"File exists: {file_path}")
+        result = self.executor.execute_code_in_docker(submission_id, language, submission_folder)
 
-
-        result = self.executor.execute_code_in_docker(job_id, language, user_folder)
-
-        return {"job_id": job_id, "status": SubmissionStatus.COMPLETED.value, "result": result}
+        return {"submission_id": submission_id, "state": SubmissionState.COMPLETED.value, "result": result}
 
 
 
 submission_handler = SubmissionHandler()
 
+class SubmissionRequest(BaseModel):
+    problem_id: str
+    lang: str
+    files_metadata: dict
+
 @router.post("/submit/")
-async def submit_code(
-    files: list[UploadFile] = File(...),
-    language: str = Form(...),
-    user_id: str = Form(...),
-    problem_id: str = Form(...)
-):
-    result = await submission_handler.handle_submission(files, language, user_id, problem_id)
-    return JSONResponse(result)
+async def submit_code(submission: SubmissionRequest):
+    try:
+        result = await submission_handler.handle_submission(submission.files_metadata, submission.lang, "1", submission.problem_id) # Hardcoding the user_id for now. Should be fetched from the JWT token
+        return JSONResponse(result)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
