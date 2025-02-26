78cdfff51fa54b069ad2f6d6c0f72424f6500393
sujal0320 <99661343+sujal0320@users.noreply.github.com>
2025-02-26 21:10:37 +0530

    Remove app. from files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.routes import submission_handler
+from routes import submission_handler
 import os
-from app.config import UPLOAD_DIR 
+from config import UPLOAD_DIR
 
 
 app = FastAPI(
@@ -18,7 +18,3 @@ app.include_router(submission_handler.router, prefix="/api/v1", tags=["Submissio
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Code Execution Platform!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
