49d185282352eac6a9417bfc4774ac6d70b5d741
sujal0320 <99661343+sujal0320@users.noreply.github.com>
2025-02-28 01:33:29 +0530

    Make docker changes in deployment in task definition

--- app/main.py
+++ app/main.py
@@ -27,3 +27,6 @@ app.include_router(submission_handler.router, prefix="/api/v1", tags=["Submissio
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Code Execution Platform!"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
