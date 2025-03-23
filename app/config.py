8e1e32b64cff8217a6f360564cd2abd4e3f832f7
sujal0320 <99661343+sujal0320@users.noreply.github.com>
2025-03-23 16:09:21 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,30 @@
 import os
+import boto3
+from botocore.exceptions import ClientError
 
+
+def get_secret():
+
+    secret_name = "lldfySecrets"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
 UPLOAD_DIR = "submission_uploads"
 LANGUAGE_CONFIG = {
     "java": {"dockerfile": "Dockerfile.java", "image": "java_executor"},
@@ -19,3 +44,5 @@ CLIENT_ID = os.getenv("COGNITO_CLIENT_ID")
 CLIENT_SECRET = os.getenv("COGNITO_CLIENT_SECRET")
 DB_PORT = "5432"
 SQLALCHEMY_DB_HOST = os.getenv("SQLALCHEMY_DB_HOST")
+
+Modify code and update os.getenv to secret = get_secret_value_response['SecretString']
