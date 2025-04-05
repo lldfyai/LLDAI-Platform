96ac11e18ca5ef890a775d2796f4b79db3f51964
sujal0320 <99661343+sujal0320@users.noreply.github.com>
2025-04-05 19:47:07 +0530

    Update userSchema.py

--- app/graphqls/userSchema.py
+++ app/graphqls/userSchema.py
@@ -1,6 +1,6 @@
 from ariadne import make_executable_schema, load_schema_from_path
-from graphqlSchema.userResolvers import resolvers
+from graphqls.userResolvers import resolvers
 import os
-schema_path = os.path.join(os.path.dirname(__file__), "userSchema.graphql")
+schema_path = os.path.join(os.path.dirname(__file__), "schema/userSchema.graphql")
 type_defs = load_schema_from_path(schema_path)
 userSchema = make_executable_schema(type_defs, resolvers)
