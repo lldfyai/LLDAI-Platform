69ee9314d671ac23dd16a62561430d9dd33f2396
sujal0320 <99661343+sujal0320@users.noreply.github.com>
2025-05-31 02:17:35 +0530

    Update tables.py

--- app/models/db/tables.py
+++ app/models/db/tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, Enum
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP, Enum, ForeignKey
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import ARRAY
@@ -10,11 +10,14 @@ class UserMetadata(Base):
     __tablename__ = 'UserMetadata'
     userId = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(255), nullable=False)
-    userName = Column(String(255), nullable=True)  # Nullable as per schema
+    userName = Column(String(255), nullable=True)  # Nullable as per schema # Default to CURRENT_TIMESTAMP
+
+class UserSubmissionStats(Base):
+    __tablename__ = 'UserSubmissionStats'
+    userId = Column(Integer, primary_key=True, autoincrement=True)
     problemsSolved = Column(Integer, nullable=True, default=0, server_default='0')
     rank = Column(Integer, nullable=True, default=0, server_default='0')
-    created_at = Column(TIMESTAMP, nullable=True, server_default=func.now())  # Default to CURRENT_TIMESTAMP
-
+    created_at = Column(TIMESTAMP, nullable=True, server_default=func.now()) 
 class ProblemMetadata(Base):
     __tablename__ = 'ProblemMetadata'
     problemId = Column(Integer, primary_key=True, autoincrement=True)
@@ -23,3 +26,14 @@ class ProblemMetadata(Base):
     timeLimit = Column(Float, nullable=True)  # Double precision in PostgreSQL
     memoryLimit = Column(Float, nullable=True)  # Double precision in PostgreSQL
     tags = Column(ARRAY(String), nullable=False, default=[], server_default='{}')  # Array of strings with default '{}'
+class UserStats(Base):
+    __tablename__ = "UserStats"
+
+    userId = Column(
+        Integer,
+        ForeignKey("UserMetadata.user_id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    totalSolved    = Column(Integer, nullable=False, default=0)
+    acceptanceRate = Column(Integer, nullable=False, default=0)
+    rating          = Column(Integer, nullable=False, default=0)
